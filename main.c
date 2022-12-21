bccfabfff68d1df99cfa142f3d12e8c631278c88
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-12-21 15:25:25 +0900

    /./

--- main.c
+++ main.c
@@ -18,8 +18,29 @@
 
 #define TIME_HIDE           2
 
+int isMet(int patient_n, int number)
+                {
+                    ifct_element = ifctdb_getData(patient_n);
+					//i번째=ifctele_getinfestedTime(patient_n)-N_HISTORY-i;
+					ifct_element = ifctdb_getData(patient_n);
+					ifctele_getinfestedTime(patient_n)-ifctele_getinfestedTime(number)<N_HISTORY;
+					//계산된 시점에서의 대상환자 이동장소 계산 
+				}
 
-int trackInfester(int patient_no, int *detected_time, int *place);
+int trackInfester(int patient_no, int *detected_time, int *place)
+{
+    for(i=0;i<pIndex;i++)
+	{
+		만난시간=isMet(현재환자,patient_no)
+		if(만난시간>0) //만났다면
+		{
+			if(지금까지 환자 중 만난 시간이 가장 이름가?)
+			   전파자=i; 
+		} 
+	}
+	return 전파자; 
+}
+
 int main(int argc, const char * argv[]) {
     
     int menu_selection;
@@ -117,20 +138,20 @@ int main(int argc, const char * argv[]) {
             case MENU_TRACK:
             	int input_index;
 				printf("환자번호를 입력하세요:");
-			    scanf("%d",&input_index);
+			    scanf("%d",&input_index);  // 현재환자를 입력받아라 
 			    while(input_index<pIndex)
 			    {
-			    	
+			    	ifct_element = ifctdb_getData(input_index);
+			    	influencer=trackInfester(input_index,ifctele_getinfestedTime(ifct_element),ifctele_getHistPlaceIndex(input_index, int index));
+			    	if(influencer>0)
+			    	    printf("%i 환자는 %i 환자에게 전파됨\n",input_index,influencer);
+			    	else
+			    	    First_influencer=influencer; //최초 전파자=현재환자
+					input_index=influencer; //현재환자=전파자 
+						 
 				}
 				 
-                int isMet(int patient_n, int number)
-                {
-                    ifct_element = ifctdb_getData(patient_n);
-					//i번째=ifctele_getinfestedTime(patient_n)-N_HISTORY-i;
-					ifct_element = ifctdb_getData(patient_n);
-					ifctele_getinfestedTime(patient_n)-ifctele_getinfestedTime(number)<N_HISTORY;
-					//계산된 시점에서의 대상환자 이동장소 계산 
-				}
+                
                 break;
                 
             default:
